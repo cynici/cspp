f095f2e623476c3ed87d2a4880551fb7f5d03477
Cheewai Lai <clai@csir.co.za>
2017-07-06 08:43:12 +0200

    Revert to use bash in docker-entrypoint.sh because it is a prerequisite

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
-#! /bin/sh
-
+#! /bin/bash
+#
+# bash is a prerequisite
+#
 set -ux
-
-HOME="${RUNUSER_HOME:-/home/runuser}"
-export HOME
+export HOME="${RUNUSER_HOME:-/home/runuser}"
 
 if [ -x /usr/sbin/useradd ]; then
   useradd -s /bin/false --no-create-home --home-dir "$HOME" -u $RUNUSER_UID runuser
@@ -12,9 +12,24 @@ else
 fi
 
 set +u
-if [ -d "$HOME/CSPP" ] ; then
-  source $HOME/.bashrc
-else
+test -f "$HOME/.bashrc" || {
+  cat >$HOME/.bashrc <<EOF
+export CSPP_SDR_HOME=\$HOME/CSPP/SDR_2_2
+source \$CSPP_SDR_HOME/cspp_sdr_env.sh
+
+export CSPP_EDR_HOME=\$HOME/CSPP/EDR_2_0
+source \$CSPP_EDR_HOME/cspp_edr_env.sh
+
+export CSPP_GTM_HOME=\$HOME/CSPP/GTM_2_0
+source \$CSPP_GTM_HOME/cspp_gtm_env.sh
+
+export POLAR2GRID_HOME=\$HOME/CSPP/polar2grid_v_2_0
+source \$POLAR2GRID_HOME/bin/polar2grid_env.sh
+EOF
+  echo "Created $HOME/.bashrc"
+}
+source $HOME/.bashrc
+test -d "$HOME/CSPP" || {
   echo "**** Download and extract CSPP package into $HOME/CSPP/" >&2
-fi
+}
 exec gosu runuser "$@"
