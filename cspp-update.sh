7aee24c08d02b107368929eaf02f15a4f686ba6f
fatcat <afisuser@fatcat.dhcp.meraka.csir.co.za>
2019-04-04 09:20:24 +0200

    fix: Add error check to cspp-update.sh

--- cspp-update.sh
+++ cspp-update.sh
@@ -5,5 +5,9 @@
 #
 # Recommended daily amcillary files update for CSPP v3.1.2
 set -eu
+[ -f /home/runuser/.bashrc ] && [ -d /home/runuser/CSPP ] || {
+    echo "CSPP not set up or missing from /home/runuser/CSPP" >&2
+    exit 1
+}
 source /home/runuser/.bashrc
 sdr_ancillary.sh -d -W /home/runuser/tmp
